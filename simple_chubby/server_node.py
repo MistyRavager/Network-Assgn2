03d7c3fc5ff13dcf578d5fc03876f8f4cbcc4519
MistyRavager <kushagra.gupta0103@gmail.com>
2023-12-03 17:10:10 +0530

    made socket for server node

--- simple_chubby/server_node.py
+++ simple_chubby/server_node.py
@@ -3,6 +3,8 @@ import sys
 import os
 import time
 import threading
+import socket
+import json
 
 host = 'localhost'
 port = 5000
@@ -15,12 +17,51 @@ def check_timeout():
         server.timeout()
         time.sleep(server.timeout_limit/2)
 
+'''
+    client sends message in the form of a json string:
+    {
+        "type": "ack"/"request",
+        "id": "id"
+    }
+'''
+def parse_message(string):
+    global server
+    message = json.loads(string)
+    if message['type'] == 'ack':
+        server.set_ack(message['id'])
+    elif message['type'] == 'request':
+        if server.handle_request(message['id']):
+            print('Server granted request to', message['id'])
+        else:
+            print('Server denied request to', message['id'])
+    else:
+        print('Invalid message type')
+
 threading.Thread(target=check_timeout).start()
 
 try:
     while True:
-        # get message from a requestor
-        msg = []
+        
+        # listen to incoming connections using sockets
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, port))
+        s.listen(5) # maybe make this only 1 connection at a time?
+        print('Server listening....')
+
+        # accept connection
+        conn, addr = s.accept()
+        print('Got connection from', addr)
+
+        # receive data from client
+        data = conn.recv(1024)
+        if not data:
+            break
+        else:
+            parse_message(data.decode('utf-8'))
+        conn.close()
+        s.close()
+
+
 
 except KeyboardInterrupt:
     print('Interrupted')
