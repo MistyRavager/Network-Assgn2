a858224ad6c0442c24b010762a7886550cd2ab76
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2023-12-04 18:34:43 +0530

    ++

--- mininet_script.py
+++ mininet_script.py
@@ -2,10 +2,10 @@ from mininet.topo import Topo
 
 class Star(Topo):
     def build(self):
-        q = [self.addHost(f'h{i}') for i in range(4)]
-        s = [self.addSwitch(f's{i}') for i in range(4)]
-        l = [self.addLink(f'h{i}', f's{i}') for i in range(4)]
+        q = [self.addHost(f'h{i}') for i in range(5)]
+        s = [self.addSwitch(f's{i}') for i in range(5)]
+        l = [self.addLink(f'h{i}', f's{i}') for i in range(5)]
         ss = self.addSwitch('s0x')
-        ssl = [self.addLink(f's{i}', 's0x') for i in range(4)]
+        ssl = [self.addLink(f's{i}', 's0x') for i in range(5)]
 
 topos = {'star': (lambda: Star())}
