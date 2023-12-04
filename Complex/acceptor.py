0ff556f68dfcf4494acc97cbaff25ed78ba28013
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2023-12-04 18:15:18 +0530

    Acceptors class-compatible

--- Complex/acceptor.py
+++ Complex/acceptor.py
@@ -1,27 +1,32 @@
-from Complex.messages_acc import *
+import json
+from classes import *
+import dataclasses
 import threading
+from typing import List, Tuple
+from socket import *
+from dir import *
 
 class Acceptor():
-    def __init__(self, id) -> None:
+    def __init__(self, id: int) -> None:
         self.id = id
-        self.ballot_num = -1 # Uninit
-        self.accepted = [] # List of (ballot_num, slot, command).
+        self.ballot_num = Ballot(-1, -1) # Uninit
+        self.accepted: List[Tuple[int, Ballot, Command]] = [] # List of (slot, ballot_num, command).
 
         self.lock_accepted = threading.Lock() # Sync things
         self.lock_ballot_num = threading.Lock()
 
-    def receive_proposal(self, message: Prepare):
+    def receive_proposal(self, message: P1A):
         """
         Phase 1.
         Input is prepare, output is a promise
         """
         with self.lock_ballot_num:
-            if message.ballot_num > self.ballot_num:
+            if message.ballot > self.ballot_num:
                 self.ballot_num = message.ballot_num
-            return Promise(message.sender_id, self.id, self.ballot_num, self.accepted)
+            return P1B(MessageType.P1B, message.ballot.leader_id, self.id, self.ballot_num, self.accepted)
         # TODO: UDP packet size < accepted
 
-    def receive_accept_request(self, message: AcceptRequest):
+    def receive_accept_request(self, message: P2A):
         """
         Phase 2
         Input is accept-request, output is an ack
@@ -29,18 +34,38 @@ class Acceptor():
         with self.lock_accepted:
             with self.lock_ballot_num:
                 if message.ballot_num == self.ballot_num:
-                    self.accepted += [(message.ballot_num, message.slot, message.command)]
-            return Ack(message.sender_id, self.id, self.ballot_num)
+                    self.accepted += [(message.slot, message.ballot_num, message.command)]
+                return P2B(MessageType.P2B, message.leader_id, self.id, self.ballot_num)
         
     def handle(self, message: Message):
         """
             Handles received message from proposer.
         """
-        if type(message) is Prepare:
+        if message.type == MessageType.P1A:
             msg = self.receive_proposal(message)
         
         # If accept-request message received
-        elif type(message) is AcceptRequest:
+        elif message.type == MessageType.P2A:
             msg = self.receive_accept_request(message)
         
         return msg
+    
+    def listen(self):
+        ipp = acceptors[self.id]
+        sock = socket(AF_INET, SOCK_DGRAM)
+        sock.bind(ipp)
+
+        while True:
+            data, addr = sock.recvfrom(1024)
+            msg = json.loads(data.decode('ascii'))
+            msg = Message.from_json(msg)
+
+            # print(f"{self.id}: Received {msg} from {addr}")
+
+            res = self.handle(msg)
+
+            # print(f"{self.id}: Sending {msg} to {addr}")
+            
+            sock.sendto(json.dumps(dataclasses.asdict(res)).encode('ascii'), addr)
+            
+            # print(f"{self.id}: Sent {msg} to {addr}")
--- Complex/classes.py
+++ Complex/classes.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 from typing import List, Dict, Tuple
 from enum import Enum
 
+@dataclass_json
 @dataclass
 class Command:
     client_id: int
@@ -24,38 +26,47 @@ class MessageType(Enum):
     P2B = 8
 
 @dataclass
+@dataclass_json
 class Message:
     type: MessageType
 
+
+@dataclass_json
 @dataclass
 class Request(Message):
     command: Command
 
 @dataclass
+@dataclass_json
 class Response(Message):
     command: Command
     result: str
 
 @dataclass
+@dataclass_json
 class Propose(Message):
     slot: int
     command: Command
 
 @dataclass
+@dataclass_json
 class Decision(Message):
     slot: int
     command: Command
 
 @dataclass(order=True)
+@dataclass_json
 class Ballot:
     num: int
     leader_id: int
 
 @dataclass
+@dataclass_json
 class P1A(Message):
     ballot: Ballot
 
 @dataclass
+@dataclass_json
 class P1B(Message):
     leader_id: int
     acceptor_id: int
@@ -63,6 +74,7 @@ class P1B(Message):
     accepted: List[Tuple[int, Ballot, Command]]
 
 @dataclass
+@dataclass_json
 class P2A(Message):
     leader_id: int
     ballot: Ballot
@@ -70,6 +82,7 @@ class P2A(Message):
     command: Command
 
 @dataclass
+@dataclass_json
 class P2B(Message):
     leader_id: int
     acceptor_id: int
